654b187381a97739761ab141cc73dc2ab0c62dbe
Maxime Chéramy <maxime@codingame.com>
2017-12-19 15:52:40 +0100

    update for game engine 1.5

--- src/main/resources/view/config.js
+++ src/main/resources/view/config.js
@@ -1,12 +1,5 @@
 import { GraphicEntityModule } from './entity-module/GraphicEntityModule.js';
 
-export const assets = {
-  baseUrl: '',
-  images: {
-    background: '/assets/Background.jpg'
-  }
-};
-
 export const modules = [
 	GraphicEntityModule
 ];
