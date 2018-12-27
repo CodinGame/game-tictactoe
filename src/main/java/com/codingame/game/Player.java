db5cc69b22350a622bfd9fab10ba7b8ce316ac17
Tzupy <tzupie@gmail.com>
2018-12-27 12:47:46 +0200

    Updated the SDK to 2.15. Also fixes issue #2.

--- src/main/java/com/codingame/game/Player.java
+++ src/main/java/com/codingame/game/Player.java
@@ -1,8 +1,8 @@
 package com.codingame.game;
 
-import com.codingame.gameengine.core.AbstractPlayer;
+import com.codingame.gameengine.core.AbstractMultiplayerPlayer;
 
-public class Player extends AbstractPlayer {
+public class Player extends AbstractMultiplayerPlayer {
     @Override
     public int getExpectedOutputLines() {
         return 1;
